283ca096bd5d6148e49d898f4f1aafeaa5e61817
Sys Admin <sysadmin@centotech.com>
2026-05-20 16:51:32 +0530

    added db port

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
