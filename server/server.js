4de70b7832ca987fe0120e805cb4ce7f69102b68
Sys Admin <sysadmin@centotech.com>
2026-05-20 16:31:59 +0530

    fixed cors issue

--- server/server.js
+++ server/server.js
@@ -10,7 +10,13 @@ const app = express();
    MIDDLEWARE
 ========================= */
 
-app.use(cors());
+app.use(
+ cors({
+   origin: "*",
+   methods: ["GET", "POST", "PUT", "DELETE"],
+   allowedHeaders: ["Content-Type"],
+ })
+);
 app.use(express.json());
 
 /* =========================
